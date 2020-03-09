6d2c2a924bb8046cf25fde22fc970ce0cbbbc02d
tv-reddy <tvreddy@ucdavis.edu>
2020-03-09 14:08:01 -0700

    add lock
    to make consumers wait until producer finished generating work

--- cuda/cdpSimpleQuicksort/0_Simple/cdpSimpleQuicksort/quicksort_workstealing_linked_list.cu
+++ cuda/cdpSimpleQuicksort/0_Simple/cdpSimpleQuicksort/quicksort_workstealing_linked_list.cu
@@ -47,7 +47,7 @@ struct Deque {
 
 // to push the task on to the work queue
 __device__ 
-void push(Deque_t queue, Task_t newTask) 
+void push(Deque_t queue, Task newTask) 
 {   
     if (queue->tail < MAX_TASKS)
     {
@@ -154,142 +154,150 @@ __device__ void selection_sort(unsigned int *data, int left, int right)
 __global__ void cdp_simple_quicksort(unsigned int *data, int left, int right, int depth)
 {
     
-    if (blockIdx.x == 0)
-    {
-        // create deque for this kernel call in global memory
-        __global__ Deque_t queue;
-
-        // allocate memory for the deque
-        queue = (Deque_t)malloc(sizeof(struct Deque));
-        // TODO: Initialize the pointers and tasks array for the queue
-        queue->head.index = 0;
-        queue->head.ctr = 0;
-        queue->tail = 0;
-        // If we're too deep or there are few elements left, we use an insertion sort...
-        if (depth >= MAX_DEPTH || right-left <= INSERTION_SORT)
+    volatile bool lock = True;
+    
+    while(lock){
+        if (blockIdx.x == 0)
         {
-            selection_sort(data, left, right);
-            return;
-        }
+            // create deque for this kernel call in global memory
+            __global__ Deque_t queue;
 
-        unsigned int *lptr = data+left;
-        unsigned int *rptr = data+right;
-        unsigned int  pivot = data[(left+right)/2];
-
-        // Do the partitioning.
-        while (lptr <= rptr)
-        {
-            // Find the next left- and right-hand values to swap
-            unsigned int lval = *lptr;
-            unsigned int rval = *rptr;
-
-            // Move the left pointer as long as the pointed element is smaller than the pivot.
-            while (lval < pivot)
+            // allocate memory for the deque
+            queue = (Deque_t)malloc(sizeof(struct Deque));
+            // TODO: Initialize the pointers and tasks array for the queue
+            queue->head.index = 0;
+            queue->head.ctr = 0;
+            queue->tail = 0;
+            // If we're too deep or there are few elements left, we use an insertion sort...
+            if (depth >= MAX_DEPTH || right-left <= INSERTION_SORT)
             {
-                lptr++;
-                lval = *lptr;
+                selection_sort(data, left, right);
+                return;
             }
 
-            // Move the right pointer as long as the pointed element is larger than the pivot.
-            while (rval > pivot)
+            unsigned int *lptr = data+left;
+            unsigned int *rptr = data+right;
+            unsigned int  pivot = data[(left+right)/2];
+
+            // Do the partitioning.
+            while (lptr <= rptr)
             {
-                rptr--;
-                rval = *rptr;
+                // Find the next left- and right-hand values to swap
+                unsigned int lval = *lptr;
+                unsigned int rval = *rptr;
+
+                // Move the left pointer as long as the pointed element is smaller than the pivot.
+                while (lval < pivot)
+                {
+                    lptr++;
+                    lval = *lptr;
+                }
+
+                // Move the right pointer as long as the pointed element is larger than the pivot.
+                while (rval > pivot)
+                {
+                    rptr--;
+                    rval = *rptr;
+                }
+
+                // If the swap points are valid, do the swap!
+                if (lptr <= rptr)
+                {
+                    *lptr++ = rval;
+                    *rptr-- = lval;
+                }
             }
 
-            // If the swap points are valid, do the swap!
-            if (lptr <= rptr)
+            // Now the recursive part
+            int nright = rptr - data;
+            int nleft  = lptr - data;
+
+            // Launch a new block to sort the left part.
+            if (left < (rptr-data))
             {
-                *lptr++ = rval;
-                *rptr-- = lval;
+                // cudaStream_t s;
+                // cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
+                // cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
+                // cudaStreamDestroy(s);
+
+                Task task1;
+                task1 = (Task_t)malloc(sizeof(struct Task));
+                // TODO: point to the subarray 
+                task1->elements = data;
+                // TODO: left and right limit
+                task1->left = left;
+                task1->right = nright;
+                // TODO: depth
+                task1->depth = depth + 1;
+
+                // TODO: push this task to the queue
+                push(queue, task1);
             }
+
+            // Launch a new block to sort the right part.
+            if ((lptr-data) < right)
+            {
+                // cudaStream_t s1;
+                // cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+                // cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
+                // cudaStreamDestroy(s1);
+
+                Task task2;
+                task2 = (Task_t)malloc(sizeof(struct Task));
+                // TODO: point to the subarray 
+                task2->elements = data;
+                // TODO: left and right limit
+                task2->left = nleft;
+                task2->right = right;
+                // TODO: depth
+                task2->depth = depth + 1;
+                // TODO: push this task to the queue
+                push(queue, task2);
+            }
+
+            // the parent block pops the first task
+            // TODO: pop the task
+            pop(queue);
+            // TODO: launch the task
+            cudaStream_t s;
+            cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
+            cdp_simple_quicksort<<< 2, 1, 0, s >>>(data, left, nright, depth+1);
+            cudaStreamDestroy(s);
+
+            // free the lock
+            lock = false;
         }
 
-        // Now the recursive part
-        int nright = rptr - data;
-        int nleft  = lptr - data;
-
-        // Launch a new block to sort the left part.
-        if (left < (rptr-data))
-        {
-            // cudaStream_t s;
-            // cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
-            // cdp_simple_quicksort<<< 1, 1, 0, s >>>(data, left, nright, depth+1);
-            // cudaStreamDestroy(s);
-
-            Task_t task1;
-            task1 = (Task_t)malloc(sizeof(struct Task));
-            // TODO: point to the subarray 
-            task1->elements = data;
-            // TODO: left and right limit
-            task1->left = left;
-            task1->right = nright;
-            // TODO: depth
-            task1->depth = depth + 1;
-
-            // TODO: push this task to the queue
-            push(queue, task1);
-        }
-
-        // Launch a new block to sort the right part.
-        if ((lptr-data) < right)
-        {
-            // cudaStream_t s1;
-            // cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-            // cdp_simple_quicksort<<< 1, 1, 0, s1 >>>(data, nleft, right, depth+1);
-            // cudaStreamDestroy(s1);
-
-            Task_t task2;
-            task2 = (Task_t)malloc(sizeof(struct Task));
-            // TODO: point to the subarray 
-            task2->elements = data;
-            // TODO: left and right limit
-            task2->left = nleft;
-            task2->right = right;
-            // TODO: depth
-            task2->depth = depth + 1;
-            // TODO: push this task to the queue
-            push(queue, task2);
-        }
-
-        // the parent block pops the first task
-        // TODO: pop the task
-        pop(queue);
-        // TODO: launch the task
-        cudaStream_t s;
-        cudaStreamCreateWithFlags(&s, cudaStreamNonBlocking);
-        cdp_simple_quicksort<<< 2, 1, 0, s >>>(data, left, nright, depth+1);
-        cudaStreamDestroy(s);
     }
 
     // second task is stolen by the consumer
-    if(blockIdx.x == 1)
-    {
-        // TODO: steal the task
-        Task task = NULL;
-        int ATTEMPTS = 10;
-        for(int i=0; i < ATTEMPTS; i++) {
-            task = steal(queue);
-            if (task.elements > 0) {
-                break;
-            }
-        }
 
-        if(task.elements > 0) {
-            // TODO: launch the task
-            cudaStream_t s1;
-            cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
-            cdp_simple_quicksort<<< 2, 1, 0, s1 >>>(task.elements, task.left, task.right, task.depth+1);
-            cudaStreamDestroy(s1);
-        } else {
-            return;
+    // TODO: steal the task
+    Task task = NULL;
+    int ATTEMPTS = 10;
+    for(int i=0; i < ATTEMPTS; i++) {
+        task = steal(queue);
+        if (task.elements > 0) {
+            break;
         }
     }
+
+    if(task.elements > 0) {
+        // TODO: launch the task
+        cudaStream_t s1;
+        cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+        cdp_simple_quicksort<<< 2, 1, 0, s1 >>>(task.elements, task.left, task.right, task.depth+1);
+        cudaStreamDestroy(s1);
+    } else {
+        return;
+    }
+
 
     // TODO: freeup the memory
     // free(task1);
     // free(task2);
     free(deque);
+    
 }
 
 ////////////////////////////////////////////////////////////////////////////////
